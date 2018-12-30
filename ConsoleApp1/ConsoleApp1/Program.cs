bac6591ce61d40febeaf55879c5127e46ddf2cfd
David Edelstein <yitzedelstein@gmail.com>
2018-12-30 18:48:30 -0500

    used a for loop to increment through arrays in game is won method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace TICTACTOE
             Console.WriteLine("Hello World!");
             var g = new Tictacto();
             g.GameIsWon();
+
             Console.ReadKey();
         }
     }
@@ -19,7 +20,7 @@ namespace TICTACTOE
     {
         public Tictacto()
         {
-            bool[] Line1 = new bool [3] { true, false, true };
+            bool[] Line1 = new bool [3] { true, true, true };
             bool[] Line2 = new bool [3] { true, false, true };
             bool[] Line3 = new bool [3] { true, false, true };
             GameBoard = new bool[3][] { Line1, Line2, Line3 };
@@ -30,16 +31,30 @@ namespace TICTACTOE
         //LOOK AT THE BOARD AND RETURN TRUE IF THERES A WINNER AND FALSE IF NO WINNER
         public bool GameIsWon()
         {
-            if (GameBoard[0][0] == true && GameBoard[0][1] == true && GameBoard[0][2] == true)
+            for (int i = 0; i < GameBoard.Length; i++)
             {
-                Console.WriteLine("The game is won!");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("This is not a win");
+                var row = GameBoard[i];
+                if (row[0] == row[1] == row[2])
+                {
+                    Console.WriteLine("The game is won");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("This is not a win");
+                }
             }
             return false;
+            //if (GameBoard[0][0] == true && GameBoard[0][1] == true && GameBoard[0][2] == true)
+            //{
+            //    Console.WriteLine("The game is won!");
+            //    return true;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This is not a win");
+            //}
+            //return false;
         }
     }
 
